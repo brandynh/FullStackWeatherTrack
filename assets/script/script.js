1585d7936d3fcb7692f162b7700b295a3aff8d70
Brandyn <hufferbrandyn@gmail.com>
2021-09-20 03:13:38 -0500

    Add fetch for user city input, add previous search chips

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,10 +5,15 @@ const todayHumidityEl = document.getElementById('todayHumidity');
 const todayWindSpeedEl = document.getElementById('todayWindSpeed');
 const todayUVEl = document.getElementById('todayUV');
 const cityInputEl = document.getElementById('city');
+const cityFormEl = document.getElementById('city-form');
+const searchCardEl = document.getElementById('searchCard');
 const myWeatherKey = "4f5d3556a1b8d12b4e6fd29d3701426e"
 
 var nearLat;
 var nearLon;
+
+var userCityLat;
+var userCityLon;
 
 nearbyButtonEl.addEventListener('click', getNearbyWeatherInfo);
 trackButtonEl.addEventListener('click', getWeatherByCity);
@@ -49,7 +54,7 @@ function getNearbyWeatherInfo() {
         });
 
     function nearMeWeather() {
-        var nearWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + nearLat + '&lon=' + nearLon + '&units=metric&appid='+myWeatherKey;
+        var nearWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + nearLat + '&lon=' + nearLon + '&units=metric&appid=' + myWeatherKey;
 
         fetch(nearWeatherUrl)
             .then(function (response) {
@@ -58,7 +63,7 @@ function getNearbyWeatherInfo() {
                         displayTodaysWeather(data);
 
                         for (var i = 0; i < data.daily.length; i++) {
-                            console.log(data.daily[i].temp.day * 1.8 + 32);
+                            console.log(Math.round(data.daily[i].temp.day * 1.8 + 32));
                         }
                         console.log(data)
                     });
@@ -69,7 +74,14 @@ function getNearbyWeatherInfo() {
             }).catch(function (error) {
                 alert('Error');
             });
-            
+
+    }
+
+    function displayTodaysWeather(data) {
+        todayTempEl.textContent = "Temperature: " + (Math.round(data.current.temp * 1.8 + 32));
+        todayHumidityEl.textContent = "Humidity: " + (data.current.humidity) + "%";
+        todayWindSpeedEl.textContent = "Wind Speed: " + (data.current.wind_speed) + "mph";
+        todayUVEl.textContent = "UV Index: " + (data.current.uvi);
     }
 
 }
@@ -77,26 +89,48 @@ function getNearbyWeatherInfo() {
 function getWeatherByCity(event) {
     event.preventDefault();
 
-    var city = cityInputEl.value.trim();
-    console.log("Test= " + city);
-    if (city) {
-        console.log(city);
+    var userCity = cityInputEl.value.trim();
+    console.log("User Input= " + userCity);
+    if (userCity) {
+        console.log(userCity);
         byCityWeather();
+        userPreviousCity(userCity);
 
     } else {
         alert('Please enter a city');
     }
 
-    
+
 
     function byCityWeather() {
-        var userCityUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=metric&appid='+myWeatherKey;
+        var userCityUrl1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&units=metric&appid=' + myWeatherKey;
 
-        fetch(userCityUrl)
+        fetch(userCityUrl1)
             .then(function (response) {
                 if (response.ok) {
                     response.json().then(function (data) {
-                        console.log(city + ' tempature: ' + (data.main.temp * 1.8 + 32));
+                        displayTodaysCityWeather(data);
+                        userCityLat = data.coord.lat;
+                        userCityLon = data.coord.lon;
+
+                        var userCityUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + userCityLat + '&lon=' + userCityLon + '&appid=' + myWeatherKey;
+
+                        fetch(userCityUrl2)
+                            .then(function (response) {
+                                if (response.ok) {
+                                    response.json().then(function (data) {
+                                        displayTodaysUVI(data);
+                                        console.log(userCityLat);
+                                        console.log(userCityLon);
+
+                                    });
+
+                                } else {
+                                    alert('Error: ' + response.statusText);
+                                }
+                            }).catch(function (error) {
+                                alert('Error');
+                            });
                     });
 
                 } else {
@@ -105,13 +139,37 @@ function getWeatherByCity(event) {
             }).catch(function (error) {
                 alert('Error');
             });
-            
+
+
+    }
+    function displayTodaysCityWeather(data) {
+        todayTempEl.textContent = "Temperature: " + (Math.round(data.main.temp * 1.8 + 32));
+        todayHumidityEl.textContent = "Humidity: " + (data.main.humidity) + "%";
+        todayWindSpeedEl.textContent = "Wind Speed: " + (data.wind.speed) + "mph";
+    }
+
+    function displayTodaysUVI(data) {
+        todayUVEl.textContent = "UV Index: " + (data.current.uvi);
+    }
+
+    function userPreviousCity(userCity) {
+        var createDiv = document.createElement('div');
+        var createChipClass = document.createAttribute('class');
+        var createId = document.createAttribute('id');
+        var createBr = document.createElement('br');
+
+        createChipClass.value = "chip";
+        createId.value = "historyChip";
+        createDiv.setAttributeNode(createChipClass);
+        createDiv.setAttributeNode(createId);
+        createDiv.innerText = userCity;
+
+        if(searchCardEl.lastChild.innerText !== userCity ) {
+        
+        searchCardEl.appendChild(createBr);
+        searchCardEl.appendChild(createDiv);
+    }
+
     }
 }
 
-function displayTodaysWeather(data) {
-    todayTempEl.textContent = "Temperature: " + (data.current.temp * 1.8 + 32);
-    todayHumidityEl.textContent = "Humidity: " + (data.current.humidity) + "%";
-    todayWindSpeedEl.textContent ="Wind Speed: " + (data.current.wind_speed) + "mph";
-    todayUVEl.textContent ="UV Index: " + (data.current.uvi);
-}
